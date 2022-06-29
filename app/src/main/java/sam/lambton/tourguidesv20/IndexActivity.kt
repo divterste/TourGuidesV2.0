e546330a3274b2d0f628506653e6f8a07e8a703d
Divyan <divyanterancestephencanada@gmail.com>
2022-06-29 15:25:57 -0400

    git init git remote add origin https://github.com/divterste/TourGuidesV2.0.git git add . git commit -m "first commit" git push -u origin master

--- app/src/main/java/sam/lambton/tourguidesv20/IndexActivity.kt
+++ app/src/main/java/sam/lambton/tourguidesv20/IndexActivity.kt
@@ -2,6 +2,9 @@ package sam.lambton.tourguidesv20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class IndexActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
