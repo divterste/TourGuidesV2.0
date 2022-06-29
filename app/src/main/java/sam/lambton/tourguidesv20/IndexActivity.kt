9bc6b2e73412838a9a7e60bf26a3e8a49119f695
Jankiba <janki7397@gmail.com>
2022-06-29 09:57:55 -0500

    Jankiba

--- app/src/main/java/sam/lambton/tourguidesv20/IndexActivity.kt
+++ app/src/main/java/sam/lambton/tourguidesv20/IndexActivity.kt
@@ -1,14 +1,24 @@
 package sam.lambton.tourguidesv20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class IndexActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index)
+
+        val summerCard : CardView = findViewById(R.id.summerCard)
+
+        summerCard.setOnClickListener {
+            //Toast.makeText(this, "Summer clicked", Toast.LENGTH_LONG).show()
+            val intent = Intent(applicationContext, SummerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
