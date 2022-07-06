a22e0bee877ed164b1ac7d4974ad718ce4d76337
Jankiba <janki7397@gmail.com>
2022-07-06 16:57:57 -0500

    //janki

--- app/src/main/java/sam/lambton/tourguidesv20/WinterActivity.kt
+++ app/src/main/java/sam/lambton/tourguidesv20/WinterActivity.kt
@@ -1,11 +1,37 @@
 package sam.lambton.tourguidesv20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WinterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_winter)
+
+        val Banffbutton : Button = findViewById(R.id.BtnBanff)
+        val Louisebutton : Button = findViewById(R.id.BtnLouise)
+        val Edmontonbutton : Button = findViewById(R.id.BtnEdmonton)
+
+        Banffbutton.setOnClickListener {
+            //Toast.makeText(this, "Summer clicked", Toast.LENGTH_LONG).show()
+            val intent = Intent(applicationContext, BanffActivity::class.java)
+            startActivity(intent)
+        }
+
+        Louisebutton.setOnClickListener {
+            //Toast.makeText(this, "Summer clicked", Toast.LENGTH_LONG).show()
+            val intent = Intent(applicationContext, LouiseActivity::class.java)
+            startActivity(intent)
+        }
+
+        Edmontonbutton.setOnClickListener {
+            //Toast.makeText(this, "Summer clicked", Toast.LENGTH_LONG).show()
+            val intent = Intent(applicationContext, EdmontonActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
